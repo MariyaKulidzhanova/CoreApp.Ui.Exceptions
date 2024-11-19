206f7d38493f2d098e528c8a7e3e7484289968d8
Mariia Kulidzhanova <mkulidzhanova@axmor.com>
2024-11-19 21:30:02 +0300

    Add retryThenLogCommand and its handler

---
+++ Proglib.Exceptions/RetryThenLogCommand.cs
@@ -0,0 +1,35 @@
+﻿namespace Proglib.Exceptions
+{
+    public class RetryThenLogCommand : ICommand
+    {
+        private readonly ICommand _command;
+        private readonly int _maxRetries;
+
+        public RetryThenLogCommand(ICommand command, int maxRetries = 2)
+        {
+            _command = command;
+            _maxRetries = maxRetries;
+        }
+
+        public void Execute()
+        {
+            int attempts = 0;
+            while (attempts < _maxRetries)
+            {
+                try
+                {
+                    _command.Execute();
+                    return;
+                }
+                catch
+                {
+                    attempts++;
+                    if (attempts == _maxRetries)
+                    {
+                        throw new InvalidOperationException($"Command failed after {attempts} retries.");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Proglib.Exceptions/RetryThenLogExceptionHandler.cs
@@ -0,0 +1,21 @@
+﻿namespace Proglib.Exceptions
+{
+    public class RetryThenLogExceptionHandler : IExceptionHandler
+    {
+        private readonly Queue<ICommand> _commandQueue;
+
+        public RetryThenLogExceptionHandler(Queue<ICommand> commandQueue)
+        {
+            _commandQueue = commandQueue;
+        }
+
+        public void Handle(ICommand failedCommand, Exception ex)
+        {
+            var retryThenLogCommand = new RetryThenLogCommand(failedCommand);
+            _commandQueue.Enqueue(retryThenLogCommand);
+
+            var logCommand = new LogCommand(ex);
+            _commandQueue.Enqueue(logCommand);
+        }
+    }
+}
